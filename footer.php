2a44990e66daea43df104f4daab7d6b03cd60446
unknown <khoirul_93@hotmail.com>
2020-07-02 08:25:41 +0700

    Web susu Ettana

---
+++ footer.php
@@ -0,0 +1,35 @@
+<footer class="footer bg-gradient-dark">
+    <div class="row">
+        <div class="col-md-4">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.478290854159!2d110.53793261477847!3d-7.844904994347537!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a4f6fc657bc3d%3A0xa5827d8e341335b!2sKelompok%20Tani%20Ternak%20PE%20PURBAYA!5e0!3m2!1sid!2sid!4v1591334284986!5m2!1sid!2sid" width="200" height="200" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            <h5>Alamat Toko</h5>
+            <p>embung, Nglanggeran Wetan, Nglanggeran, Kec. Patuk, Kabupaten Gunung Kidul, Daerah Istimewa Yogyakarta 55862</p>
+        </div>
+        <div class="col-md-4">
+            <a href=""><img src="img/Ettana.png" width="100"></a>
+            <p></p>
+            <h8>Kami Juga ada di :</h8>
+            <p></p>
+            <a href="https://www.bukalapak.com/u/chocoindustries"><img src="img/bl.png" width="100"></a>
+            <p></p>
+            <a href="https://www.tokopedia.com/chocoindustries"><img src="img/tokped.png" width="75"></a>
+            <p></p>
+            <a href="https://shopee.co.id/tonnitauhidd"><img src="img/shope.png" width="50"></a>
+            <p></p>
+            <p class="copyright"><small>
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                    Copyright &copy;
+                    <script>
+                        document.write(new Date().getFullYear());
+                    </script> Design <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://instagram.com/khoirul93" target="_blank">Khoirul</a>
+                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                </small></p>
+        </div>
+        <div class="col-md-4">
+            <a href="https://api.whatsapp.com/send?phone=6282138037647&text=Halo%20admin%20saya%20kesulitan%20memesan%20produk" target="_blank"><img src="img/wa.png" width="75" alt="maps"></a>
+            <h5>Bantuan</h5>
+            <p>Silahkan Klik logo whatsapp di bawah ini untuk menghubungi admin</p>
+        </div>
+    </div>
+
+</footer>
---
+++ koneksi.php
@@ -0,0 +1,1 @@
+<?php $koneksi = new mysqli("localhost", "susuett1_wp930", "-1Uc]7pSU4", "susuett1_wp930");
